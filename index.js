7a5909805e56835ff55e2a0776751e502829b452
alatos2 <alabitosin60@gmail.com>
2019-07-19 14:20:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// database connection
+connectDB();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`server started at port ${PORT}`));
